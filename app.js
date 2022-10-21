078c3580650d57ba5864bd935263f408777d6927
Pandu <Justikapandu@gmail.com>
2022-10-21 23:26:08 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+var expressLayouts = require('express-ejs-layouts');
+const app =  express()
+
+app.listen(3000, ()=>{
+    console.log("server run on port 3000")
+})
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+app.get('/about',(req,res)=>{
+    res.render('about', {
+        title: 'about',
+        layout: 'layouts/main-layout'
+    })
+} )
